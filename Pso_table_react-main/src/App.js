86fce66a154713b41941abe0c6962b015a351e46
Vardhaman Begani <beganivardhaman0110@gmai.com>
2024-03-17 02:41:58 +0530

    pso table ui work+ fixing

---
+++ Pso_table_react-main/src/App.js
@@ -0,0 +1,22 @@
+
+import "./App.css";
+
+
+import PsoTable from "./Screens/Psotable";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <>
+      <div className="overflow-hidden bg-light">
+        <Header />
+        <PsoTable />
+      </div>
+    
+    
+    </>
+  );
+}
+
+export default App;
---
+++ Pso_table_react-main/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div style={{textAlign:"center"}}>
+        <h2>DY Patil College Of Engineering , Akurdi , Pune.</h2>
+        <h1 style={{color:"#a30c0d"}}>Department Of Artificial Intelligence & Data Science </h1>
+        <h3>A.Y 2023-24 SEM-VI</h3>
+    </div>
+  )
+}
+
+export default Header
